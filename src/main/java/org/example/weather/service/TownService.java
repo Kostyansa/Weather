f0c062557c62a182006ed2197b3ff799279a9d50
Kostyansa <kostyanovozhilov@mail.ru>
2020-04-01 09:59:03 +0300

    Added get all function

--- src/main/java/org/example/weather/service/TownService.java
+++ src/main/java/org/example/weather/service/TownService.java
@@ -5,13 +5,19 @@ import org.example.weather.entity.Town;
 import org.example.weather.repository.TownRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class TownService {
 
     private final TownRepository townRepository;
 
-    Town get(String town){
+    public List<Town> get(){
+        return townRepository.get();
+    }
+
+    public Town get(String town){
         return townRepository.getByName(town);
     }
 }
